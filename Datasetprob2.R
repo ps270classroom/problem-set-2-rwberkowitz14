a8c0fddfd14df35c68d613906b8a2d58edc62913
Reuben Berkowitz <rwberkowitz@wisc.edu>
2024-09-24 21:44:29 -0500

    Saving other info

---
+++ Datasetprob2.R
@@ -0,0 +1,13 @@
+ Name               | Description
+  | `cand_id`          | unique identifier code for candidate                                 |
+  | `cand_name`        | full name of the candidate                                           |
+  | `cand_name_last`   | last name of the candidate                                           |
+  | `party`            | party affiliation of the candidate (R = Republican, D = Democrat)    |
+  | `office`           | office being sought by candidate                                     |
+  | `state`            | state in which the candidate is running                              |
+  | `incumbency`       | incumbency status of candidate (incumbent, challenger, or open seat) |
+  | `spend`            | estimated total spending on Facebook ads by candidate                |
+  | `impressions`      | estimated total impressions of Facebook ads                          |
+  | `ad_tone_attack`   | proportion of FB ads that mention candidate's opponent only          |
+| `ad_tone_promote`  | proportion of FB ads that mention candidate only                     |
+| `ad_tone_contrast` | proportion of FB ads that mention candidate and candidate's opponent |
